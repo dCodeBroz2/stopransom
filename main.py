667f5b832b091614aa2cd5f1ceade979e9da3a38
dcodebroz <dcodebroz@bitbucket.org>
2020-08-25 16:40:18 +0200

    STEPS explained

--- main.py
+++ main.py
@@ -4,4 +4,8 @@ if __name__ == '__main__':
     w = watchDOG.watchDOG()
     w.run()
   
-#TODO: Implement watching for ransomWare object extensions.
+#TODO: 
+# STEP 1- Write core functionality, the ransom class and class watchdog detector
+# STEP 2- Write agent module, for windows, mac and linux. bind and reverse
+# STEP 3- Write GUI for the app, admin should be able to view stats and stuff like that
+# STEP 4- Write prevention mechanisms, this should order agent to do something.
